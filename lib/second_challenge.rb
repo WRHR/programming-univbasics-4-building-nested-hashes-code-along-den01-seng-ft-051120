31a4e017211d4a97f3ed61a7218a102645fdd0f6
Will Reeves <wrhreeves@gmail.com>
2020-04-08 21:03:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patiarch => {},
+      :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => {}
